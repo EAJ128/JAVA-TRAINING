17042670be11298838b0efb153c776af3532ce12
EAJ128 <eaj281@gmail.com>
2026-03-24 18:26:24 +0530

    DAY 4

--- DAY 4/oops.java
+++ DAY 4/oops.java
@@ -1,18 +1,16 @@
-class student{
-    int rollno;
+class student
+{
     String name;
     int marks;
     boolean bool;
     student(int roll_no,String na_me,int mark_s)
     {
-        rollno = roll_no;
         name = na_me;
         marks = mark_s;
         System.out.println("parameterized constructor");
     }
     student(int rollno,String name,int marks,boolean bool)
     {
-        this.rollno = rollno;
         this.name = name;
         this.marks = marks;
         this.bool = bool;
@@ -31,4 +29,4 @@ public class oops {
         System.out.println(s3.bool);
     }
 }
-}
+
