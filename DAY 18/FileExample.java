1a5a49345c8f4c4b9185bf546a61020463acfd1c
EAJ128 <eaj281@gmail.com>
2026-04-15 14:37:32 +0530

    DAY 18

---
+++ DAY 18/FileExample.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+class FileExample
+{
+    public static void main(String[] args)
+    {
+        File newFile=new File("test.txt");
+        try {
+            if(newFile.createNewFile())
+            {
+                System.out.println("File created: "+newFile.getName());
+            }
+            else
+            {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
